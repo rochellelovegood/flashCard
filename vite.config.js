5093ca6f67fb32cadc256e6911d8cae6081bc4bc
Khin Than Thar <115642358+rochellelovegood@users.noreply.github.com>
2023-11-14 09:59:04 +0630

    nosvg

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default{
+    base:'/',
+    build:{
+        rollupOptions:{
+            input:{
+                'main':'index.html',
+                'script':'script.js'
+            }
+        }
+    }
+}
